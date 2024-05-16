4dd610707797a309d00ccda1ddcfe1be3d3b6b49
Alexander Riedel <54716527+alexriedel1@users.noreply.github.com>
2024-05-16 14:49:10 +0200

    mask normal

--- data/ad_dataset.py
+++ data/ad_dataset.py
@@ -86,7 +86,7 @@ class DefaultAD(data.Dataset):
 		img = Image.open(img_path).convert("RGB")
 		img = to_dtype(to_image(img), torch.float32, scale=True)
 		if anomaly == 0:
-			img_mask = Image.fromarray(np.zeros((img.size[0], img.size[1])), mode='L')
+			img_mask = Mask(torch.zeros(image.shape[-2:])).to(torch.uint8)
 		else:
 			mask_path = f'{self.root}/{mask_path}'
 			img_mask = Image.open(mask_path).convert("L")
